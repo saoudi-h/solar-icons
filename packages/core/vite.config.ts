44ab026a5d2815bd4ac48112a888aae18dc5d019
Hakim Saoudi <saoudihakim@gmail.com>
2024-11-05 14:21:00 +0100

    feat(core): add build config

---
+++ packages/core/vite.config.ts
@@ -0,0 +1,53 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'node:path'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
+import pkg from './package.json'
+
+export default defineConfig({
+    plugins: [
+        viteStaticCopy({
+            targets: [
+                {
+                    src: 'src/svgs',
+                    dest: '.',
+                },
+                {
+                    src: 'src/metadata.json',
+                    dest: '.',
+                },
+            ],
+        }),
+    ],
+    build: {
+        minify: true,
+        target: 'ES2017',
+        lib: {
+            entry: resolve(__dirname, 'src/index.ts'),
+        },
+        rollupOptions: {
+            external: Object.keys(pkg.peerDependencies),
+            input: './src/index.ts',
+            output: [
+                {
+                    format: 'esm',
+                    dir: 'dist/esm',
+                    preserveModules: true,
+                    preserveModulesRoot: 'src',
+                    entryFileNames: '[name].mjs',
+                },
+                {
+                    format: 'cjs',
+                    dir: 'dist/cjs',
+                    preserveModules: true,
+                    preserveModulesRoot: 'src',
+                    entryFileNames: '[name].cjs',
+                },
+            ],
+        },
+    },
+    resolve: {
+        alias: {
+            '@': '/src',
+        },
+    },
+})
