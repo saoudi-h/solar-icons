4923539693d495dc8a4bb4b015e11a91973a3a39
Hakim Saoudi <saoudihakim@gmail.com>
2024-11-07 16:07:07 +0100

    refactor(react): Update eslint.config.js and add tsconfig.build.json

--- packages/react/eslint.config.js
+++ packages/react/eslint.config.js
@@ -1,5 +1,20 @@
 import { configs, defineConfig } from '@solar/eslint'
 
-export default defineConfig(...configs.base, {
-    ignores: ['eslint.config.js', 'eslint-types.d.ts', 'prettier.config.js'],
-})
+export default defineConfig(
+    ...configs.base,
+    {
+        ignores: ['eslint.config.js', 'eslint-types.d.ts', 'prettier.config.js'],
+    },
+    {
+        rules: {
+            '@typescript-eslint/no-unused-vars': [
+                'error',
+                {
+                    varsIgnorePattern: '^React$',
+                    argsIgnorePattern: '^_',
+                    ignoreRestSiblings: true,
+                },
+            ],
+        },
+    }
+)
