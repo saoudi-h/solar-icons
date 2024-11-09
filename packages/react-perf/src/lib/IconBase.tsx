a07968d3d323f8e88b720f35f736146f95be0bc3
Hakim Saoudi <saoudihakim@gmail.com>
2024-11-09 04:59:37 +0100

    feat(react-perf): Add lib files

---
+++ packages/react-perf/src/lib/IconBase.tsx
@@ -0,0 +1,33 @@
+import React, { forwardRef } from 'react'
+import type { IconProps } from './types'
+
+const IconBase = forwardRef<SVGSVGElement, IconProps>((props, ref) => {
+    const {
+        alt,
+        color = 'currentColor',
+        size = '1em',
+        mirrored = false,
+        children,
+        ...restProps
+    } = props
+
+    return (
+        <svg
+            ref={ref}
+            xmlns="http://www.w3.org/2000/svg"
+            width={size}
+            height={size}
+            color={color}
+            fill="none"
+            viewBox="0 0 24 24"
+            transform={mirrored ? 'scale(-1, 1)' : undefined}
+            {...restProps}>
+            {!!alt && <title>{alt}</title>}
+            {children}
+        </svg>
+    )
+})
+
+IconBase.displayName = 'IconBase'
+
+export default IconBase
