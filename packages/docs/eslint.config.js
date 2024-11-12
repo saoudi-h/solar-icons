a780791277d68b6802f2e5f8e3533541bd47c796
Hakim Saoudi <saoudihakim@gmail.com>
2024-11-12 02:33:26 +0100

    chore(docs): format:fix

--- packages/docs/eslint.config.js
+++ packages/docs/eslint.config.js
@@ -1,8 +1,5 @@
 import { configs, defineConfig } from '@solar-icons/eslint'
 
-export default defineConfig(
-    ...configs.next,
-    {
-        ignores: ['eslint.config.js', 'eslint-types.d.ts', 'prettier.config.js'],
-    },
-)
+export default defineConfig(...configs.next, {
+    ignores: ['eslint.config.js', 'eslint-types.d.ts', 'prettier.config.js'],
+})
