e941b3f2923abcb0dab28536ca20efce0bea5114
Hakim Saoudi <saoudihakim@gmail.com>
2024-11-14 03:46:03 +0100

    feat(docs): Add IconContainer and NoiseSvg components for UI enhancements

---
+++ packages/docs/components/ui/icon-container.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const IconContainer: React.FC<{ children: React.ReactNode }> = ({ children }) => (
+    <div className="rounded-md border to-secondary shadow-md  bg-gradient-to-t from-fd-background/80 p-1 ">
+        {children}
+    </div>
+)
---
+++ packages/docs/components/ui/noise-svg.tsx
@@ -0,0 +1,30 @@
+import React from "react"
+
+const NoiseSvg = React.forwardRef<SVGSVGElement, React.SVGProps<SVGSVGElement>>((props, ref) => (
+    <svg {...props} ref={ref}>
+        <filter id="noise-filter">
+            <feTurbulence
+                type="fractalNoise"
+                baseFrequency="0.54"
+                numOctaves="4"
+                stitchTiles="stitch"></feTurbulence>
+            <feColorMatrix type="saturate" values="0"></feColorMatrix>
+            <feComponentTransfer>
+                <feFuncR type="linear" slope="0.61"></feFuncR>
+                <feFuncG type="linear" slope="0.61"></feFuncG>
+                <feFuncB type="linear" slope="0.61"></feFuncB>
+                <feFuncA type="linear" slope="1"></feFuncA>
+            </feComponentTransfer>
+            <feComponentTransfer>
+                <feFuncR type="linear" slope="3" intercept="-1.00" />
+                <feFuncG type="linear" slope="3" intercept="-1.00" />
+                <feFuncB type="linear" slope="3" intercept="-1.00" />
+            </feComponentTransfer>
+        </filter>
+        <rect width="100%" height="100%" filter="url(#noise-filter)"></rect>
+    </svg>
+))
+
+NoiseSvg.displayName = 'NoiseSvg'
+
+export { NoiseSvg }
