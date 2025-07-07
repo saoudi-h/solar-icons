0b2d0533c936d1962eec9b277b814fbd98e95980
Hakim Saoudi <saoudihakim@gmail.com>
2025-07-07 16:27:53 +0200

    feat(docs): add og images to doc pages

--- apps/docs/app/docs/[[...slug]]/page.tsx
+++ apps/docs/app/docs/[[...slug]]/page.tsx
@@ -1,52 +1,56 @@
-import { source } from '@/lib/source';
-import {
-  DocsPage,
-  DocsBody,
-  DocsDescription,
-  DocsTitle,
-} from 'fumadocs-ui/page';
-import { notFound } from 'next/navigation';
-import { createRelativeLink } from 'fumadocs-ui/mdx';
-import { getMDXComponents } from '@/mdx-components';
+import { source } from '@/lib/source'
+import { DocsPage, DocsBody, DocsDescription, DocsTitle } from 'fumadocs-ui/page'
+import { notFound } from 'next/navigation'
+import { createRelativeLink } from 'fumadocs-ui/mdx'
+import { getMDXComponents } from '@/mdx-components'
 
-export default async function Page(props: {
-  params: Promise<{ slug?: string[] }>;
-}) {
-  const params = await props.params;
-  const page = source.getPage(params.slug);
-  if (!page) notFound();
+export default async function Page(props: { params: Promise<{ slug?: string[] }> }) {
+    const params = await props.params
+    const page = source.getPage(params.slug)
+    if (!page) notFound()
 
-  const MDXContent = page.data.body;
+    const MDXContent = page.data.body
 
-  return (
-    <DocsPage toc={page.data.toc} full={page.data.full}>
-      <DocsTitle>{page.data.title}</DocsTitle>
-      <DocsDescription>{page.data.description}</DocsDescription>
-      <DocsBody>
-        <MDXContent
-          components={getMDXComponents({
-            // this allows you to link to other pages with relative file paths
-            a: createRelativeLink(source, page),
-          })}
-        />
-      </DocsBody>
-    </DocsPage>
-  );
+    return (
+        <DocsPage toc={page.data.toc} full={page.data.full}>
+            <DocsTitle>{page.data.title}</DocsTitle>
+            <DocsDescription>{page.data.description}</DocsDescription>
+            <DocsBody>
+                <MDXContent
+                    components={getMDXComponents({
+                        // this allows you to link to other pages with relative file paths
+                        a: createRelativeLink(source, page),
+                    })}
+                />
+            </DocsBody>
+        </DocsPage>
+    )
 }
 
 export async function generateStaticParams() {
-  return source.generateParams();
+    return source.generateParams()
 }
 
-export async function generateMetadata(props: {
-  params: Promise<{ slug?: string[] }>;
-}) {
-  const params = await props.params;
-  const page = source.getPage(params.slug);
-  if (!page) notFound();
+export async function generateMetadata(props: { params: Promise<{ slug?: string[] }> }) {
+    const { slug = [] } = await props.params
+    const page = source.getPage(slug)
+    if (!page) notFound()
 
-  return {
-    title: page.data.title,
-    description: page.data.description,
-  };
+    const image = {
+        url: ['/og', ...slug, 'image.png'].join('/'),
+        width: 1200,
+        height: 630,
+    }
+
+    return {
+        title: page.data.title,
+        description: page.data.description,
+        openGraph: {
+            url: `/docs/${page.slugs.join('/')}`,
+            images: [image],
+        },
+        twitter: {
+            images: [image],
+        },
+    }
 }
---
+++ apps/docs/app/og/[...slug]/og.tsx
@@ -0,0 +1,135 @@
+import { ImageResponse } from 'next/og'
+import type { ReactElement, ReactNode } from 'react'
+import type { ImageResponseOptions } from 'next/dist/compiled/@vercel/og/types'
+import { LogoIcon } from '@/components/ui-blocks/logo/LogoIcon'
+
+interface GenerateProps {
+    title: ReactNode
+    description?: ReactNode
+    primaryTextColor?: string | undefined
+}
+
+export function generateOGImage(options: GenerateProps & ImageResponseOptions): ImageResponse {
+    const { title, description, primaryTextColor, ...rest } = options
+
+    return new ImageResponse(
+        generate({
+            title,
+            description,
+            primaryTextColor,
+        }),
+        {
+            width: 1200,
+            height: 630,
+            ...rest,
+        }
+    )
+}
+
+export function generate({
+    primaryTextColor = 'rgb(255,150,255)',
+    ...props
+}: GenerateProps): ReactElement {
+    return (
+        <div
+            style={{
+                overflow: 'hidden',
+                borderRadius: '5rem',
+                position: 'relative',
+                fontFamily: 'body',
+                display: 'flex',
+                flexDirection: 'column',
+                width: '100%',
+                height: '100%',
+                color: 'white',
+                backgroundColor: 'rgb(10,10,10)',
+            }}>
+            <svg
+                style={{
+                    filter: 'blur(1px)',
+                    position: 'absolute',
+                    inset: '0',
+                    width: '100%',
+                    height: '100%',
+                    opacity: 0.5,
+                }}>
+                <filter id="noise-filter">
+                    <feTurbulence
+                        type="fractalNoise"
+                        baseFrequency="30.54"
+                        numOctaves="10"
+                        stitchTiles="stitch"></feTurbulence>
+                    <feColorMatrix type="saturate" values="0"></feColorMatrix>
+                    <feComponentTransfer>
+                        <feFuncR type="linear" slope="0.61"></feFuncR>
+                        <feFuncG type="linear" slope="0.61"></feFuncG>
+                        <feFuncB type="linear" slope="0.61"></feFuncB>
+                        <feFuncA type="linear" slope="1"></feFuncA>
+                    </feComponentTransfer>
+                    <feComponentTransfer>
+                        <feFuncR type="linear" slope="3" intercept="-1.00" />
+                        <feFuncG type="linear" slope="3" intercept="-1.00" />
+                        <feFuncB type="linear" slope="3" intercept="-1.00" />
+                    </feComponentTransfer>
+                </filter>
+                <rect width="100%" height="100%" filter="url(#noise-filter)"></rect>
+            </svg>
+            <div
+                style={{
+                    position: 'absolute',
+                    top: '0',
+                    left: '50%',
+                    transform: 'translateX(-50%) translateY(-50%)',
+                    borderRadius: '9999px',
+                    width: '66.666667%',
+                    height: '66.666667%',
+                    backgroundColor: 'transparent',
+                    filter: 'blur(128px)',
+                    background: 'linear-gradient(180deg, hsl(235, 44%, 59%) 0%, transparent 100%)',
+                }}></div>
+            <div
+                style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    width: '100%',
+                    height: '100%',
+                    padding: '4rem',
+                }}>
+                <p
+                    style={{
+                        fontWeight: 700,
+                        fontSize: '76px',
+                        fontFamily: 'heading',
+                    }}>
+                    {props.title}
+                </p>
+                <p
+                    style={{
+                        fontSize: '48px',
+                        color: 'rgba(240,240,240,0.7)',
+                    }}>
+                    {props.description}
+                </p>
+                <div
+                    style={{
+                        display: 'flex',
+                        flexDirection: 'row',
+                        alignItems: 'center',
+                        gap: '24px',
+                        marginTop: 'auto',
+                        color: primaryTextColor,
+                    }}>
+                    <LogoIcon width={64} height={64} color={'hsl(235 44% 59%)'} />
+                    <p
+                        style={{
+                            fontSize: '46px',
+                            fontWeight: 700,
+                            fontFamily: 'heading',
+                        }}>
+                        Solar Icons
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
