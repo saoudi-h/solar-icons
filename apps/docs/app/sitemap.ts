c10373c23aaae3cad54fb4b68b1114ac844cca11
Hakim Saoudi <saoudihakim@gmail.com>
2025-07-08 00:37:34 +0200

    feat(docs): add sitemap generation

---
+++ apps/docs/app/sitemap.ts
@@ -0,0 +1,37 @@
+import type { MetadataRoute } from 'next';
+import { source } from '@/lib/source';
+import { baseUrl } from '@/lib/metadata';
+
+export const revalidate = false;
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const url = (path: string): string => new URL(path, baseUrl).toString();
+
+  return [
+    {
+      url: url('/'),
+      changeFrequency: 'monthly',
+      priority: 1,
+    },
+    {
+      url: url('/icons'),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: url('/docs'),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    ...(await Promise.all(
+      source.getPages().map(async (page) => {
+        return {
+          url: url(page.url),
+          lastModified: page.data.lastModified ? new Date(page.data.lastModified) : undefined,
+          changeFrequency: 'weekly',
+          priority: 0.5,
+        } as MetadataRoute.Sitemap[number];
+      }),
+    )),
+  ];
+}
