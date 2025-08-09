6b57377e671d0508e6f4567ff503ff89f70a3822
Hakim Saoudi <saoudihakim@gmail.com>
2025-08-09 15:22:38 +0200

    feat(nuxt): add category alias and auto-import improvements
    - Add #solar-icons/category alias support

--- packages/nuxt/src/module.ts
+++ packages/nuxt/src/module.ts
@@ -51,14 +51,13 @@ export default defineNuxtModule<SolarNuxtModuleOptions>({
   async setup(options, nuxt) {
     const resolver = createResolver(import.meta.url)
 
-    // Configuration des alias pour la résolution des modules
     nuxt.options.alias['#solar-icons'] = '@solar-icons/vue'
     nuxt.options.alias['#solar-icons/lib'] = '@solar-icons/vue/lib'
+    nuxt.options.alias['#solar-icons/category'] = '@solar-icons/vue/category'
 
     addTypeTemplate({
       filename: 'types/solar-icons.d.ts',
       getContents: () => `
-          // Types pour les alias Solar Icons
           declare module '#solar-icons' {
             export * from '@solar-icons/vue'
           }
@@ -66,10 +65,13 @@ export default defineNuxtModule<SolarNuxtModuleOptions>({
           declare module '#solar-icons/lib' {
             export * from '@solar-icons/vue/lib'
           }
+          
+          declare module '#solar-icons/category' {
+            export * from '@solar-icons/vue/category'
+          }
         `,
     })
 
-    // Auto-import des icônes
     if (options.autoImport) {
       const iconNames = await getAllIconNames()
       iconNames.forEach((iconName) => {
@@ -81,20 +83,17 @@ export default defineNuxtModule<SolarNuxtModuleOptions>({
       })
     }
 
-    // Composants de base
     addComponent({
       name: 'SolarProvider',
       export: 'SolarProvider',
       filePath: '@solar-icons/vue/lib',
     })
 
-    // Composable
     addImports({
       name: 'useSolar',
       from: '@solar-icons/vue/lib',
     })
 
-    // Provider global
     if (options.provider) {
       nuxt.options.runtimeConfig.public.solarIcons = {
         config: {
--- packages/nuxt/types/aliases.d.ts
+++ packages/nuxt/types/aliases.d.ts
@@ -5,3 +5,7 @@ declare module '#solar-icons' {
 declare module '#solar-icons/lib' {
   export * from '@solar-icons/vue/lib'
 }
+
+declare module '#solar-icons/category' {
+  export * from '@solar-icons/vue/category'
+}
