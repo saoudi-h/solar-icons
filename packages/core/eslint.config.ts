e7b70b6b04838d9f1347dacac8cced738481c4ae
Hakim Saoudi <saoudihakim@gmail.com>
2026-02-06 19:49:06 +0100

    chore(deps): update pnpm to v10.28.2
    - Update package manager version in package.json
    - Clean up and simplify eslint config

--- packages/core/eslint.config.ts
+++ packages/core/eslint.config.ts
@@ -1,19 +1,3 @@
-// import { configs, defineConfig } from '@solar-icons/eslint'
-
-// export default (async () => {
-//     const base = await configs.base
-
-//     return defineConfig(...base, {
-//         ignores: [
-//             'eslint.config.js',
-//             'eslint-types.d.ts',
-//             'prettier.config.js',
-//             'lint-staged.config.mjs',
-//             'lowercase-script.ts',
-//         ],
-//     })
-// })()
-
 import type { Config } from '@solar-icons/eslint'
 import { base } from '@solar-icons/eslint'
 import { defineConfig } from 'eslint/config'
