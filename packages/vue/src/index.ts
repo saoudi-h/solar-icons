098d640067cfbc158018a3db2e14b4fc4e90c346
Hakim Saoudi <saoudihakim@gmail.com>
2026-06-19 20:41:47 +0200

    chore: gitignore generated icon directories across all packages
    - Add src/icons/ to react-perf, react, react-native, vue-reactive
    - Clean up vue .gitignore (remove old category.ts exceptions)
    - Update vue src/index.ts to match new unit-per-style exports

--- packages/vue/src/index.ts
+++ packages/vue/src/index.ts
@@ -1,1 +1,6 @@
-export * from './icons'
+/* GENERATED FILE */
+export type { IconProps } from "./lib/types"
+export { IconStyle } from "./lib/types"
+export * from "./icons/styled"
+import * as solar from "./icons"
+export { solar }
