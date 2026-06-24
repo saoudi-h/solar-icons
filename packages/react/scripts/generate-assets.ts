623b89b86e8b403aa29c735bab41d548197da234
Hakim Saoudi <saoudihakim@gmail.com>
2026-06-24 20:27:03 +0200

    refactor(react): remove SSR namespace export from main entry

--- packages/react/scripts/generate-assets.ts
+++ packages/react/scripts/generate-assets.ts
@@ -128,7 +128,6 @@ export type { IconProps } from "./lib"
 export { IconBase, SolarProvider, useSolar } from "./lib"
 export type { SolarProviderProps } from "./lib"
 export * from "./icons/styled"
-export * as SSR from "./icons/styled"
 `
 
     files.push({
