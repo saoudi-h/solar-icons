c71896e21286b1e71ae70bac9d239b5f69fed9b5
Hakim Saoudi <saoudihakim@gmail.com>
2026-07-14 10:32:55 +0200

    fix(js): add missing closing brace in types.ts

--- packages/js/src/types.ts
+++ packages/js/src/types.ts
@@ -1,19 +1,20 @@
 export type IconNode = [tag: string, attrs: Record<string, string>, children?: IconNode[]]
 
 export interface SVGProps extends Record<string, string | number | undefined> {
-  xmlns?: string
-  width?: string | number
-  height?: string | number
-  viewBox?: string
-  fill?: string
-  stroke?: string
-  'stroke-width'?: string | number
-  'stroke-linecap'?: string
-  'stroke-linejoin'?: string
-  class?: string
-  size?: string | number
-  color?: string
-  'secondary-color'?: string
-  'secondary-opacity'?: string | number
+    xmlns?: string
+    width?: string | number
+    height?: string | number
+    viewBox?: string
+    fill?: string
+    stroke?: string
+    'stroke-width'?: string | number
+    'stroke-linecap'?: string
+    'stroke-linejoin'?: string
+    class?: string
+    size?: string | number
+    color?: string
+    'secondary-color'?: string
+    'secondary-opacity'?: string | number
+}
 
 export type Icons = Record<string, IconNode>
